cbb88a4cc4fddaed5cfbf3940a6252bbbe0412a1
Liampobob <liam@thamlock.com>
2019-10-14 00:15:27 +0100

    Added some edge test cases

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Node.java,3/a/3ab60872f365ff6fee76b90596b6f298e0190389
+<
+src/LCA.java,a/8/a8a87a1fec3d69766bfdc57d3a6195ac5d21ef71
+@
+src/LCATest.java,7/c/7c5034afe9bb254363a9a09807be02842f24682a
--- src/LCATest.java
+++ src/LCATest.java
@@ -12,5 +12,11 @@ public class LCATest {
     public void testLowestCommonAncestor() {
         assertEquals(5, LCA.lowestCommonAncestor(head, 7, 8));
         assertEquals(1, LCA.lowestCommonAncestor(head, 2, 3));
+        assertEquals(5, LCA.lowestCommonAncestor(head, 7, 7));
+
+
+        //edge cases
+        assertEquals(Integer.MIN_VALUE, LCA.lowestCommonAncestor(head, 12, 6));
+        assertEquals(Integer.MIN_VALUE, LCA.lowestCommonAncestor(head, 6, 0));
     }
 }
